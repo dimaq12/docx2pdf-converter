4edccc46b0678408bba8d2d346c90dc49a5e6e84
dimaq12 <dimaq12@yandex.ru>
2019-01-29 23:56:24 +0200

    ziping

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const fs = require('fs');
 const toPdf = require("office-to-pdf");
+const zip = require("node-native-zip");
+
+    
 
 const app = express();
 
 app.get('/', async (req, res) => {
     const wordBuffer = fs.readFileSync("./test.docx");
     const pdfBuffer = await toPdf(wordBuffer);
-    res.set('Content-disposition', 'attachment; filename=' + 'generated-pdf' );
-    res.set('Content-type', 'application/pdf');
-    res.end( pdfBuffer );
+    const archive = new zip();
+    archive.add("generated-pdf.pdf", pdfBuffer);
+    const archiveBuffer = archive.toBuffer();
+    res.set('Content-disposition', 'attachment; filename=' + 'output.zip' );
+    res.set('Content-type', 'application/zip');
+    res.end( archiveBuffer );
 });
 
 app.set('port', process.env.PORT || 7777);
